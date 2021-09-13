c38714b1f848351310ec41607ffdc2b601d3de6f
patipan-patisampita <patipan.youminzawa@gmail.com>
2021-09-13 17:20:00 +0700

    initial commit

---
+++ js/app104.js
@@ -0,0 +1,18 @@
+let firstname = 'Michel';
+let lastName = 'Jackson';
+var fullName = firstname + " " + lastName;
+
+console.log("10" + 12);
+let a = "10";
+let b = 10;
+if(a===b){
+    console.log("ok");
+}
+else{
+    console.log("No ok");
+}
+
+console.log("5" * 5);
+console.log(5 * 5);
+console.log(fullName);
+document.write(`<h2 id = "text">${fullName}</h2>`);
---
+++ js/app106.js
@@ -0,0 +1,12 @@
+let country ="Thailand";
+let capital = "Bangkok";
+let str = "This is " + country + " " + capital;
+let str2 = `This is ${country} country and its capital is ${capital}`;
+console.log(str2);
+
+let a = 4;
+let b = 3;
+const result = `The sum of 4 + 5 is ${4 + 5}`;
+console.log(result);
+
+console.log(`${result<10 ? 'Too low':''}`);
